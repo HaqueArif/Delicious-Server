944b016126204930737f7137a5810675bc3f7690
HaqueArif <haquearif143@gmail.com>
2023-05-02 20:36:04 +0600

    created response for evry single chef details by id

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get('/chef', (req, res)=>{
     res.send(chef);
 })
 
+app.get('/chef/:id',(req, res)=>{
+    const id = req.params.id;
+    const selectedChef = chef.find(c => c.id ===id);
+    res.send(selectedChef)
+})
+
 app.listen(port, ()=>{
     console.log(`Delicious API is running on port: ${port}`);
 });
