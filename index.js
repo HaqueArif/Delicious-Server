6449b39fe87575ad1ec71f9dc9625f6d9d0c1cc7
HaqueArif <haquearif143@gmail.com>
2023-05-02 14:11:41 +0600

    fixed some error and added cors

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const popular = require('./data/popularItems.json');
+const chef = require('./data/chefInfo.json');
+
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send('Delicious is Running')
-})
+});
 
 app.get('/popular', (req, res) =>{
     res.send(popular);
+});
+
+app.get('/chef', (req, res)=>{
+    res.send(chef);
 })
 
 app.listen(port, ()=>{
     console.log(`Delicious API is running on port: ${port}`);
-})
+});
